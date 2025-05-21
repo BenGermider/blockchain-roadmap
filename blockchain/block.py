a7e312b4f1b4e1ba3ce3cff18db26a87b7d53540
BenGermider <bengermidr@gmail.com>
2025-05-21 14:49:18 +0300

    Added block and a chain

---
+++ blockchain/block.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from hashlib import sha256
+
+class Block:
+
+    def __init__(self, data, previous_hash):
+        self._data = data
+        self._previous_hash = previous_hash
+        self._hash = self._calculate_hash()
+        self._timestamp = datetime.now()
+
+    def _calculate_hash(self):
+        """
+        Calculate the hash of the block using SHA-256.
+        """
+        block_string = f"{self._data}{self._previous_hash}{self._timestamp}"
+        return sha256(block_string.encode()).hexdigest()
+
+    @property
+    def data(self):
+        """
+        Get the data of the block.
+        """
+        return self._data
+
+    @property
+    def previous_hash(self):
+        """
+        Get the previous hash of the block.
+        """
+        return self._previous_hash
---
+++ blockchain/blockchain.py
@@ -0,0 +1,22 @@
+from block import Block
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = [self._create_genesis_block()]
+
+    @staticmethod
+    def _create_genesis_block():
+        """
+        Create the genesis block (the first block in the blockchain).
+        """
+        return Block("Genesis Block", "0")
+
+    def add_block(self, data):
+        """
+        Add a new block to the blockchain.
+        """
+        previous_block = self.chain[-1]
+        new_block = Block(data, previous_block.previous_hash)
+        self.chain.append(new_block)
+
