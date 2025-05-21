3840fde40fa6c957fdcac985d1f37a2c31d7d00c
BenGermider <bengermidr@gmail.com>
2025-05-21 16:24:09 +0300

    Added async block creation

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from hashlib import sha256
 
@@ -8,6 +9,8 @@ class Block:
         self._previous_hash = previous_hash
         self._timestamp = datetime.now()
         self._hash = self._calculate_hash()
+        self._nonce = 0
+
 
     def _calculate_hash(self):
         """
@@ -29,3 +32,12 @@ class Block:
         Get the previous hash of the block.
         """
         return self._hash
+
+    async def mine_block(self, difficulty):
+        prefix = "0" * difficulty
+        while not self._hash.startswith(prefix):
+            self._nonce += 1
+            self._hash = self._calculate_hash()
+            if self._nonce % 1000 == 0:
+                await asyncio.sleep(0)
+        print(f"Block mined: {self._hash}")
--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -6,17 +6,17 @@ class Blockchain:
         self.chain = [self._create_genesis_block()]
 
     @staticmethod
-    def _create_genesis_block():
+    def _create_genesis_block() -> Block:
         """
         Create the genesis block (the first block in the blockchain).
         """
         return Block("Genesis Block", "0" * 64)
 
-    def add_block(self, data):
+    async def add_block(self, data) -> None:
         """
         Add a new block to the blockchain.
         """
         previous_block = self.chain[-1]
         new_block = Block(data, previous_block.hash)
+        await new_block.mine_block(4)  # Example difficulty
         self.chain.append(new_block)
-
